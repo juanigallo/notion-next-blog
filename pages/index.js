e4414344956af84b5bb0ab0d17d95632390757c4
Juani Gallo <ji.gallo93@gmail.com>
2022-09-16 11:12:23 -0300

    Adding visible checkbox to database

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
-import { Client } from "@notionhq/client"
-import Entry from '../components/Entry'
-import Nav from '../components/Nav'
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
+import { Client } from "@notionhq/client";
+import Entry from "../components/Entry";
+import Nav from "../components/Nav";
 
-const notion = new Client({ auth: process.env.NOTION_TOKEN })
+const notion = new Client({ auth: process.env.NOTION_TOKEN });
 
 export default function Home({ entries }) {
   return (
@@ -16,40 +16,40 @@ export default function Home({ entries }) {
       </Head>
       <Nav />
       <div className={styles.container}>
-
         <section className={styles.entries}>
-          {entries.length == 0 && 'There are no entries'}
+          {entries.length == 0 && "There are no entries"}
           {entries.map((entry, key) => {
-            return (
-              <Entry key={key} data={entry} />
-            )
+            return <Entry key={key} data={entry} />;
           })}
         </section>
       </div>
     </>
-  )
+  );
 }
 
 export async function getServerSideProps(context) {
   const entries = await notion.databases.query({
     database_id: process.env.NOTION_DATABASE_ID,
-  })
+  });
 
-  const mappedEntries = entries.results.map((entry) => {
-    const { properties } = entry
-    const { name, description, tags } = properties
+  const mappedEntries = entries.results
+    .map((entry) => {
+      const { properties } = entry;
+      const { name, description, tags, visible } = properties;
 
+      return {
+        name: name.title,
+        description: description.rich_text,
+        tags: tags.multi_select,
+        id: entry.id,
+        visible: visible.checkbox,
+      };
+    })
+    .filter((entry) => entry.visible == true);
 
-    return {
-      name: name.title,
-      description: description.rich_text,
-      tags: tags.multi_select,
-      id: entry.id
-    }
-  })
   return {
     props: {
-      entries: mappedEntries ?? []
+      entries: mappedEntries ?? [],
     },
-  }
+  };
 }
