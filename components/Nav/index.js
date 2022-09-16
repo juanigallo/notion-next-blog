e748112d281f93c82b7c5dc43fc195043f553496
Juani Gallo <ji.gallo93@gmail.com>
2022-09-15 21:29:42 -0300

    Adding logo and name into navbar

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -2,6 +2,11 @@ import styles from './style.module.scss'
 
 export default function Nav() {
   return (
-    <nav className={styles.nav}></nav>
+    <nav className={styles.nav}>
+      <div className={styles.innerNav}>
+        <div className={styles.square}></div>
+        <h1>Juani Gallo</h1>
+      </div>
+    </nav>
   )
 }
